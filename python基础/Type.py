a55e6cb24e9ed4add7012cf6798357f97079ff21
xmg <2291952482@qq.com>
2022-07-11 20:53:27 +0800

    新增py

---
+++ python基础/Type.py
@@ -0,0 +1,34 @@
+#tuple      不能修改元素
+x=(1,1.3,True,(1,2,3),[1,2,3],{"username":"xmg"})
+print(x)
+print(len(x))
+
+#下标
+print(x[0])
+print(x[-1])
+print("-----------------------------------")
+
+#list       
+
+y=[1,2,(1,2,3),[1,2,3],{"age":18}]
+y2=[1,3,4,6,1]
+print(len(y))
+print(max(y2))
+print(min(y2))
+print(y2.count(1)) #元素次数
+print(y.index([1,2,3])) #元素位置
+#列表截取
+print(y[0])
+print(y[-1])
+print(y[1:])
+print(y[1:4])
+y.append("尾部增加元素")
+y[2]=33333 #修改元素值
+y.insert(0,555) #指定位置插入元素
+y.pop(4)
+#y.extend(y2) 双表连接
+#y.clear() 删除全部元素
+y2.pop
+print("y的值为{}" .format(y))
+print("y2的值为{zhi}" .format(zhi=y2))
+
---
+++ python基础/常见方法.py
@@ -0,0 +1,27 @@
+print(2333)            #整数int
+print(2.333)           #小数float
+print(True,False)      #布尔值bool      
+print(None)            #空noneType
+print("你好")          #字符串str
+print((1,3.3,4,"你好"))       #元组 tuple
+print([1,3.3,4,"你好"])       #数组/列表 list
+print({"地址":"浙江省","年龄":18})       #字典 dict json格式"key":"value"
+
+#字符串字母大写函数:upper()   小写:lower()  首字母大写，其他小写:title() 
+#例 a="adada"  print(a.upper())
+#去除字符串空格  strip()头尾 lstrip()左边 rstrip()右边 
+"""
+多行注释
+"""
+print(1,end='')     #不换行
+print(2)
+
+print(1,end="(●'◡'●)")     
+print(2)
+
+
+type()  #数据类型   
+len()   #数据长度
+print()
+input()
+exit()  #退出
